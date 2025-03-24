c1939c86f83c9df960f8d1bdb4192c5eb030c0a3
Albin Dahlén <albin@dahlen.ws>
2025-03-24 15:33:09 +0100

    Added paste remap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -49,3 +49,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 -- vim: ts=2 sts=2 sw=2 et
+--
+-- Greatest remap of all time, paste over and delete the paste over
+vim.keymap.set('x', '<leader>p', [["_dp]])
